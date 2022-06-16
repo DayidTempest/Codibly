9297b61ae6d9fedf913fd4287b8dfd44ce9f9d21
DayidTempest <dpayerd@gmail.com>
2022-06-16 03:07:34 +0200

    delete unnecessary code

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -1,5 +1,7 @@
 package BubbleSort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -22,18 +24,9 @@ public class BubbleSort{
                 
                 Number first = input.get(j), second = input.get(j-1);
 
-                if(second.doubleValue() > first.doubleValue()){
-                    if(first.doubleValue() % first.intValue() == 0){
                         input.set(j-1, first);
-                    }else{
-                        input.set(j-1, first);
-                    }
-                    if(second.doubleValue() % second.intValue() == 0){
                         input.set(j, second);
-                    }else{
-                        input.set(j, second);
-                    }                    
-                }
+                  
             }
         }
 
